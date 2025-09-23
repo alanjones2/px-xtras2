5e730d495520b9193db7c5d46ae32c8397ffc93e
DataVizandAI <alanj2305@gmail.com>
2025-09-23 16:41:50 +0200

    1st

---
+++ README.md
@@ -0,0 +1,195 @@
+# px_xtras - a Plotly Helper Library
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+An advanced plotting library for Plotly that provides a collection of helper functions to create beautiful and informative visualizations with ease. This library simplifies the process of generating complex charts like waterfall, waffle, and dumbbell charts, allowing you to focus on your data and insights.
+
+## Overview
+
+This library provides functions for creating the following advanced chart types:
+
+*   **Waterfall Chart:** Ideal for visualizing the cumulative effect of sequential positive and negative values.
+*   **Waffle Chart:** Perfect for displaying progress toward a goal or the composition of a whole.
+*   **Dumbbell Chart:** A great way to compare two data points and visualize the change between them.
+*   **Lollipop Chart:** A visually appealing alternative to a bar chart, useful for showing the relationship between a numeric and a categorical variable.
+*   **Range Chart**: Similar to a dumbbell chart with a different aesthetic
+*   **Slope Chart**: A simplified line chart for easy comparison of two variables
+*   **Metric**: a non-graphic representation of a changing value
+
+
+## Installation
+
+To install *px_extras*, you can use pip:
+
+```bash
+pip install git+https://github.com/alanjones2/px_xtras.git
+```
+Or download the library from the repo and drop it into your project.
+
+## Usage
+
+Here are some examples of how to use the library to create different types of charts - the examples are configured as Streamlit apps.
+
+To run the apps you need to do the following imports:
+
+```python
+import streamlit as st
+import pandas as pd
+import px_xtras as ph
+```
+Below are images of the available charts. The code to create them can be found in the file [stdemo.py](stdemo.py).
+
+### Waterfall Charts
+
+![Waterfall Chart](images/waterfall1.png)
+![Waterfall Chart](images/waterfall2.png)
+
+
+### Waffle Chart
+
+![Waffle Chart](images/waffle.png)
+
+
+
+### Dumbbell Chart
+
+![Dumbbell Chart](images/dumbbell.png)
+
+
+### Vertical Dumbbell Chart
+
+![Dumbbell Chart](images/vertical-dumbbell.png)
+
+### Lollipop Chart
+
+![Lollipop Chart](images/lollipop.png)
+
+### Range chart
+
+![Range chart](images/range-chart.png)
+
+### Slope chart
+
+![Slope chart](images/slope-chart.png)
+
+### Metric
+
+![Metric](images/metric.png)
+
+## API Reference
+
+### `waterfall()`
+
+Creates a waterfall chart.
+
+**Parameters:**
+
+*   `df` (pd.DataFrame): DataFrame containing the data.
+*   `categories_col` (str): Name of the column for categories.
+*   `values_col` (str): Name of the column for values.
+*   `measure_col` (str): Name of the column for the measure type.
+*   `title` (str, optional): Title of the chart.
+*   `measure` (List[str], optional): List specifying whether each data point is 'relative' or 'total'.
+*   `color` (str, optional): A common color for all elements.
+*   `**kwargs`: Additional keyword arguments passed to `fig.update_layout()` (e.g., `width`, `height`, `template`).
+
+### `waffle()`
+
+Creates a waffle chart.
+
+**Parameters:**
+
+*   `df` (pd.DataFrame): DataFrame containing the data.
+*   `categories_col` (str): Name of the column for categories.
+*   `values_col` (str): Name of the column for values.
+*   `title` (str, optional): Title of the chart.
+*   `local_colors` (List[str], optional): A list of colors for the categories.
+*   `**kwargs`: Additional keyword arguments passed to `fig.update_layout()` (e.g., `width`, `height`, `template`).
+
+### `dumbbell()`
+
+Creates a dumbbell chart.
+
+**Parameters:**
+
+*   `df` (pd.DataFrame): DataFrame containing the data.
+*   `categories_col` (str): Name of the column for categories.
+*   `values1_col` (str): Name of the column for the first value set.
+*   `values2_col` (str): Name of the column for the second value set.
+*   `label1` (str, optional): Label for the first value set.
+*   `label2` (str, optional): Label for the second value set.
+*   `title` (str, optional): Title of the chart.
+*   `orientation` (str, optional): 'h' for horizontal (default), 'v' for vertical.
+*   `**kwargs`: Additional keyword arguments passed to `fig.update_layout()` (e.g., `width`, `height`, `template`).
+
+### `lollipop_chart()`
+
+Creates a lollipop chart.
+
+**Parameters:**
+
+*   `df` (pd.DataFrame): DataFrame containing the data.
+*   `categories_col` (str): Name of the column for categories.
+*   `values_col` (str): Name of the column for values.
+*   `title` (str, optional): Title of the chart.
+*   `stick_color` (str, optional): Color for the lollipop sticks.
+*   `marker_color` (str, optional): Color for the lollipop markers.
+*   `**kwargs`: Additional keyword arguments passed to `fig.update_layout()` (e.g., `width`, `height`, `template`).
+
+### `line_range()`
+
+Creates a line range chart.
+
+**Parameters:**
+
+*   `df` (pd.DataFrame): DataFrame containing the data.
+*   `categories_col` (str): Name of the column for categories.
+*   `values1_col` (str): Name of the column for the first value set.
+*   `values2_col` (str): Name of the column for the second value set.
+*   `title` (str, optional): Title of the chart.
+*   `orientation` (str, optional): 'h' for horizontal (default), 'v' for vertical.
+*   `**kwargs`: Additional keyword arguments passed to `fig.update_layout()` (e.g., `width`, `height`, `template`).
+
+### `metric()`
+
+Creates a metric component.
+
+**Parameters:**
+
+*   `title` (str): Label or title of the metric.
+*   `value` (float | str): Main metric value.
+*   `delta` (float | str): Optional delta/change value.
+*   `**kwargs`: Additional keyword arguments passed to `fig.update_layout()` (e.g., `width`, `height`, `template`).
+
+### `slope_chart()`
+
+Creates a slope chart.
+
+**Parameters:**
+
+*   `df` (pd.DataFrame): DataFrame containing the data.
+*   `categories_col` (str): The name of the column to be used for the x-axis.
+*   `values_cols` (List[str]): A list of column names to be plotted on the y-axis.
+*   `title` (str, optional): The title of the chart.
+*   `**kwargs`: Additional keyword arguments passed to `fig.update_layout()` (e.g., `width`, `height`, `template`).
+
+## License
+
+This project is licensed under the MIT License.
+
+## History
+
+v.0.1.0 initial version
+v0.1.0 initial release
+
+v0.1.1 - 05/08/2025
+- modify waterfall default measures to begin with absolute and end with total
+- add measures as df column
+- add default column names
+
+v0.1.2 - 04/09/2025
+- add the following:
+    - lollipop chart
+    - range chart
+    - slope chart
+    - metric
---
+++ stdemo.py
@@ -0,0 +1,198 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import px_xtras as pxx
+
+st.title("Extra Plotly Charts")
+
+st.header("Waterfall Chart - Profit Breakdown")
+wf_data = pd.DataFrame({
+    "Category": ["Revenue", "COGS", "Operating Expenses", "Taxes", "Net Profit"],
+    "Value": [100000, -50000, -20000, -10000, 0],
+})
+fig_wf = pxx.waterfall(df=wf_data, 
+                   categories_col='Category',
+                   values_col='Value',
+                   title="Company Profit Waterfall",
+                   height=400,
+                   template='plotly_dark'
+                   )
+st.plotly_chart(fig_wf)
+
+
+# --- Waterfall Chart ---
+st.header("Waterfall Chart")
+waterfall_df = pd.DataFrame({
+    'Category': ["Year beginning", "Profit1", "Loss1", "Q1", "Profit2", "Loss2", "Q2"],
+    'Value': [100, 50, -20, 0, 40, -10, 0],
+    'measure': ['absolute','relative', 'relative','total', 'relative','relative','total']
+
+})
+measure = ['absolute','relative', 'relative','total', 'relative','relative','total']
+fig_waterfall = pxx.waterfall(df=waterfall_df, 
+                             categories_col='Category', 
+                             values_col='Value', 
+                             measure_col='measure',
+                             title="Profit/Loss",
+                             color='blue'
+                             )
+st.plotly_chart(fig_waterfall)
+
+st.write("---")
+
+# --- Waffle Chart ---
+st.header("Waffle Chart")
+waffle_data = {'Others': 54, 'LD': 72, 'Conservative': 121, 'Labour': 403}
+waffle_df = pd.DataFrame(list(waffle_data.items()), columns=['Party', 'Seats'])
+
+fig_waffle = pxx.waffle(df=waffle_df, 
+                       categories_col='Party', 
+                       values_col='Seats',
+                       title='Waffle Chart of MPs',
+                       #local_colors=['black', 'yellow', 'blue', 'red']
+                       #startcolor='yellow',
+                       #endcolor='darkgreen',
+                       )
+st.plotly_chart(fig_waffle)
+
+fig_waffle = pxx.waffle(df=waffle_df, 
+                       categories_col='Party', 
+                       values_col='Seats',
+                       title='Waffle Chart of MPs',
+                       local_colors=['black', 'yellow', 'blue', 'red']
+                       #startcolor='yellow',
+                       #endcolor='darkgreen',
+                       )
+st.plotly_chart(fig_waffle)
+
+fig_waffle = pxx.waffle(df=waffle_df, 
+                       categories_col='Party', 
+                       values_col='Seats',
+                       title='Waffle Chart of MPs',
+                       #local_colors=['black', 'yellow', 'blue', 'red']
+                       startcolor='yellow',
+                       endcolor='darkgreen',
+                       width=50
+                       )
+st.plotly_chart(fig_waffle)
+
+st.write("---")
+
+# --- Dumbbell Chart ---
+st.header("Dumbbell Chart")
+dumbbell_df = pd.DataFrame({
+    'Country': ['USA', 'Canada', 'UK', 'Germany', 'France'],
+    '2020': [10, 12, 9, 14, 11],
+    '2021': [12, 15, 11, 15, 12]
+})
+
+fig_dumbbell = pxx.dumbbell(df=dumbbell_df, 
+                           categories_col='Country', 
+                           values1_col='2020', 
+                           values2_col='2021',
+                           label1='2020',
+                           label2='2021',
+                           title='Dumbbell Chart Example (Dark Template)',
+                           template='plotly_dark')
+st.plotly_chart(fig_dumbbell)
+
+# --- Vertical Dumbbell Chart ---
+st.header("Vertical Dumbbell Chart")
+fig_dumbbell_v = pxx.dumbbell(df=dumbbell_df, 
+                             categories_col='Country', 
+                             values1_col='2020', 
+                             values2_col='2021',
+                             label1='2020',
+                             label2='2021',
+                             title='Vertical Dumbbell Chart Example',
+                             orientation='v',
+                             width=500)
+st.plotly_chart(fig_dumbbell_v)
+
+fig_range = pxx.line_range(df=dumbbell_df, 
+                           categories_col='Country', 
+                           values1_col='2020', 
+                           values2_col='2021',
+                           label1='2020',
+                           label2='2021',
+                           title='Range Chart Example')
+st.plotly_chart(fig_range)
+
+
+df_life = pd.read_csv('data/lifeExp.csv').round(1)
+
+dumbbell_df = pd.DataFrame({
+    'country': ['France', 'Germany', 'Greece', 'Spain'],
+    '1952': [67.41, 67.5, 65.86, 64.94],
+    '2002': [79.59, 78.67, 78.25, 79.78]
+})
+
+
+st.dataframe(df_life)
+
+fig_dumbbell = pxx.dumbbell(df=df_life, 
+                           categories_col='country', 
+                           values1_col='1952', 
+                           values2_col='2002',
+                           label1='1952',
+                           label2='2002',
+                           title='Life expectancy chart',
+                           )
+st.plotly_chart(fig_dumbbell)
+
+fig_dumbbell2 = pxx.dumbbell(df=df_life, 
+                           categories_col='country', 
+                           values1_col='1952', 
+                           values2_col='2002',
+                           label1='1952',
+                           label2='2002',
+                           title='Life expectancy chart',
+                           orientation='v'
+                           )
+st.plotly_chart(fig_dumbbell2)
+
+fig_range = pxx.line_range(df=df_life, 
+                           categories_col='country', 
+                           values1_col='1952', 
+                           values2_col='2002',
+                           label1='1952',
+                           label2='2002',
+                           title='Range Chart Example')
+st.plotly_chart(fig_range)
+
+st.write("---")
+
+# --- Lollipop Chart ---
+st.header("Lollipop Chart")
+lollipop_df = pd.DataFrame({
+    'Category': ['A', 'B', 'C', 'D', 'E'],
+    'Value': [10, 25, 15, 30, 20]
+})
+fig_lollipop = pxx.lollipop_chart(df=lollipop_df, 
+                                 categories_col='Category', 
+                                 values_col='Value', 
+                                 title='Lollipop Chart Example (Custom Height)', height=350)
+st.plotly_chart(fig_lollipop)
+
+st.write("---")
+
+# --- Metric ---
+st.header("Metric")
+fig_metric = pxx.metric(title="Sales", value=12345, delta=123, prefix="$")
+st.plotly_chart(fig_metric)
+
+st.write("---")
+
+# --- Slope Chart ---
+st.header("Slope Chart")
+slope_df = pd.DataFrame({
+    'Year': [2020, 2021],
+    'USA': [10, 12],
+    'Canada': [12, 15],
+    'UK': [9, 11],
+    'Germany': [14, 15],
+    'France': [11, 12]
+})
+fig_slope = pxx.slope_chart(df=slope_df, categories_col='Year', values_cols=['USA', 'Canada', 'UK', 'Germany', 'France'], title='Slope Chart Example')
+st.plotly_chart(fig_slope)
